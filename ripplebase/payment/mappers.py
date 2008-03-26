d7d848c6af707110ba2a28c10f34ec695b305cdd
Ryan Fugger <rfugger@gmail.com>
2008-03-26 13:26:30 -0500

    License stuff.

--- ripplebase/payment/mappers.py
+++ ripplebase/payment/mappers.py
@@ -1,5 +1,25 @@
 "SQLAlchemy data mappers."
 
+##################
+# Copyright 2008, Ryan Fugger
+#
+# This file is part of Ripplebase.
+#
+# Ripplebase is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as 
+# published by the Free Software Foundation, either version 3 of the 
+# License, or (at your option) any later version.
+#
+# Ripplebase is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public 
+# License along with Ripplebase, in the file LICENSE.txt.  If not,
+# see <http://www.gnu.org/licenses/>.
+##################
+
 import sqlalchemy.orm as orm
 
 from tables import *
--- ripplebase/payment/pathsearch.py
+++ ripplebase/payment/pathsearch.py
@@ -1,3 +1,23 @@
+##################
+# Copyright 2008, Ryan Fugger
+#
+# This file is part of Ripplebase.
+#
+# Ripplebase is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as 
+# published by the Free Software Foundation, either version 3 of the 
+# License, or (at your option) any later version.
+#
+# Ripplebase is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public 
+# License along with Ripplebase, in the file LICENSE.txt.  If not,
+# see <http://www.gnu.org/licenses/>.
+##################
+
 from decimal import Decimal as D
 # *** Make sure decimal module is configured for right precision,
 #     and identical to DB precision.  (May have to write our own
